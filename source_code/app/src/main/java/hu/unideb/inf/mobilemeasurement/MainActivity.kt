b6dab113832bf324bb19303150f18f23b38c87b7
Péter Hegedűs <hpeti0912@gmail.com>
2021-11-06 17:36:13 +0100

    MainActivity.kt: Added replaceFragment function

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/MainActivity.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/MainActivity.kt
@@ -6,19 +6,23 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
         val navView = findViewById<NavigationView>(R.id.navView)
+
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        findViewById<DrawerLayout>(R.id.drawerLayout).addDrawerListener(toggle)
+        drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -33,6 +37,12 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
+    private fun replaceFragment(fragment: Fragment, title : String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
