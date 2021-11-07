a9363d2157d8f66661dc1e2ab08cc59d11ab8cdc
István Káplár <pistike.kaplar@gmail.com>
2021-11-07 14:40:22 +0100

    MeasureStartFragment.kt: Added radiogroups

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/MeasureStartFragment.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/MeasureStartFragment.kt
@@ -5,28 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.R
-
-
 import android.widget.RadioGroup
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.drawerlayout.widget.DrawerLayout
-
 
 class MeasureFragment : Fragment() {
 
-    lateinit var distanceGroup: RadioGroup
-    lateinit var samplingGroup: RadioGroup
-
-
+    lateinit var distanceRadioGroup : RadioGroup
+    lateinit var samplingRadioGroup: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-
-        
-        
     }
 
     override fun onCreateView(
@@ -35,9 +22,7 @@ class MeasureFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var myView = inflater.inflate(R.layout.fragment_measure_start, container, false)
-        distanceGroup = myView.findViewById<RadioGroup>(R.id.distance_Radio_Group)
-
+        distanceRadioGroup = myView.findViewById<RadioGroup>(R.id.distance_Radio_Group)
         return myView
     }
-
 }
