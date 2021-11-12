96a24f4a720c17657b846a1e9a2d93b165c92a18
Péter Hegedűs <hpeti0912@gmail.com>
2021-11-12 23:31:01 +0100

    HomeFragment.kt: Added binding and navigation action

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/home/HomeFragment.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/home/HomeFragment.kt
@@ -4,20 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import hu.unideb.inf.mobilemeasurement.R
+import hu.unideb.inf.mobilemeasurement.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater,
+            R.layout.fragment_home,
+            container,
+            false
+        )
+
+        binding.homeMeasureStartButton.setOnClickListener{ view: View ->
+            view.findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToMeasureStartFragment())
+        }
+
+        return binding.root
     }
+
 }
