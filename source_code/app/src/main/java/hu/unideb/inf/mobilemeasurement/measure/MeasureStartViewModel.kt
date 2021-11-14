c8ce5d5a7bc0c0aac0ae21af712654ab56d72a86
István Káplár <pistike.kaplar@gmail.com>
2021-11-14 22:33:49 +0100

    MeasureStartViewModel.kt: Added LiveData fields

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureStartViewModel.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureStartViewModel.kt
@@ -4,4 +4,26 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MeasureStartViewModel : ViewModel(){
+
+    val distance: MutableLiveData<Int> by lazy{
+        MutableLiveData<Int>()
+    }
+
+    val measure_ID: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+
+    val phone_ID: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
+    }
+
+    val sampling_Rate: MutableLiveData<Int> by lazy{
+        MutableLiveData<Int>()
+    }
+
+    val orientation_Data: MutableLiveData<Int> by lazy{
+        MutableLiveData<Int>()
+    }
+
+
 }
