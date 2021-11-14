d70fd4c99c88ff296359dc0fcb34940c38dcc534
István Káplár <pistike.kaplar@gmail.com>
2021-11-14 22:35:53 +0100

    MeasureStartFragment.kt: Added Listeners for inputs with LiveData

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureStartFragment.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureStartFragment.kt
@@ -7,16 +7,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import hu.unideb.inf.mobilemeasurement.R
 import hu.unideb.inf.mobilemeasurement.databinding.FragmentHomeBinding
 import hu.unideb.inf.mobilemeasurement.databinding.FragmentMeasureStartBinding
 import hu.unideb.inf.mobilemeasurement.home.HomeFragmentDirections
+import android.widget.Toast
+
+import hu.unideb.inf.mobilemeasurement.MainActivity
+
+
+
+
+
 
 class MeasureStartFragment : Fragment() {
 
     lateinit var distanceRadioGroup : RadioGroup
     lateinit var samplingRadioGroup: RadioGroup
+    lateinit var orientationRadioGroup: RadioGroup
+    lateinit var viewModel: MeasureStartViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +45,42 @@ class MeasureStartFragment : Fragment() {
             container,
             false
         )
+
+
+        distanceRadioGroup = binding.distanceRadioGroup
+        samplingRadioGroup = binding.samplingRadioGroup
+        orientationRadioGroup = binding.orientationRadioGroup
+
+        viewModel = ViewModelProvider(this).get(MeasureStartViewModel::class.java)
+
+        distanceRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                binding.distance40cm.id -> viewModel.distance.value = 40
+                binding.distance1m.id -> viewModel.distance.value = 100
+                binding.distance2m.id -> viewModel.distance.value = 200
+                binding.distance5m.id -> viewModel.distance.value = 500
+            }
+        }
+
+        samplingRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId){
+                binding.samplingA.id -> viewModel.sampling_Rate.value = 10000
+                binding.samplingB.id -> viewModel.sampling_Rate.value = 100000
+            }
+        }
+
+        orientationRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId){
+                binding.verticalButton.id -> viewModel.sampling_Rate.value = 1
+                binding.horizontalButton.id -> viewModel.sampling_Rate.value = 2
+            }
+        }
+
+        binding.phoneIDText.setText(android.os.Build.MODEL)
+
+
         binding.NextButton.setOnClickListener{ view ->
+
             view.findNavController().navigate(MeasureStartFragmentDirections.actionMeasureStartFragmentToMeasureStopFragment())
 
         }
