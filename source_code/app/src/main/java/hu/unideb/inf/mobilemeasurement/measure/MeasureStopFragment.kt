0579cfa185fe88e74939931665fcd1998d210011
Nyika-Benedek <fiokbluestacks@gmail.com>
2021-11-14 18:15:16 +0100

    MeasureStopFragment.kt: Sensor handling added for demo with data binding

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureStopFragment.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureStopFragment.kt
@@ -1,17 +1,32 @@
 package hu.unideb.inf.mobilemeasurement.measure
 
+import android.content.Context.SENSOR_SERVICE
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import hu.unideb.inf.mobilemeasurement.R
 import hu.unideb.inf.mobilemeasurement.databinding.FragmentMeasureStartBinding
 import hu.unideb.inf.mobilemeasurement.databinding.FragmentMeasureStopBinding
 
-class MeasureStopFragment : Fragment() {
+class MeasureStopFragment : Fragment(), SensorEventListener {
+
+
+    /**Sensors**/
+    private lateinit var sensorManager: SensorManager
+    private var linearAccelerometer: Sensor? = null
+    private lateinit var x_value: TextView
+    private lateinit var y_value: TextView
+    private lateinit var z_value: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +43,33 @@ class MeasureStopFragment : Fragment() {
             container,
             false
         )
-        /*
-        binding.NextButton.setOnClickListener{ view ->
-            view.findNavController().navigate(MeasureStartFragmentDirections.actionMeasureStartFragmentToMeasureStopFragment())
-
-        }*/
+        setUpSensor()
+        x_value = binding.xText
+        y_value = binding.yText
+        z_value = binding.zText
         return binding.root
+    }
+
+    /**Sensor Calcualtions**/
+    private fun setUpSensor() {
+        //sensorManager = this.context?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager = activity?.getSystemService(SENSOR_SERVICE) as SensorManager
+        //its us not ms! (1ms = 100 us)
+        linearAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION).also {
+            sensorManager.registerListener(this, it, 10000)
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_LINEAR_ACCELERATION) {
+            x_value.setText("X: " + event.values[0])
+            y_value.setText("Y: " + event.values[1])
+            z_value.setText("Z: " + event.values[2])
+        }
+
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        return
     }
 }
