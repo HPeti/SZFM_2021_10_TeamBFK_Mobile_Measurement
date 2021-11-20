a57824e8f8cf8fed3a196f4a0e502ff7767b0921
Péter Hegedűs <hpeti0912@gmail.com>
2021-11-20 23:23:36 +0100

    Added databinding and night mode switching to OptionsFragment.kt.

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/options/OptionsFragment.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/options/OptionsFragment.kt
@@ -1,13 +1,20 @@
 package hu.unideb.inf.mobilemeasurement.options
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import hu.unideb.inf.mobilemeasurement.R
+import hu.unideb.inf.mobilemeasurement.databinding.FragmentOptionsBinding
 
 class OptionsFragment : Fragment() {
+    lateinit var darkModeSwitch : Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,6 +24,29 @@ class OptionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_options, container, false)
+        val binding = DataBindingUtil.inflate<FragmentOptionsBinding>(
+            inflater,
+            R.layout.fragment_options,
+            container,
+            false
+        )
+        darkModeSwitch = binding.switch2
+
+        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            darkModeSwitch.toggle()
+        }
+
+        darkModeSwitch.setOnCheckedChangeListener { _, isChecked ->
+            Log.i("OptionsFragment", "Listener enter")
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                Log.i("OptionsFragment", "Night mode activated")
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Log.i("OptionsFragment", "Night mode disabled")
+            }
+        }
+
+        return binding.root
     }
 }
