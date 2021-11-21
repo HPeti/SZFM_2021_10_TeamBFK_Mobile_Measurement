87a045645d184f380b6e090084013786db277980
pisti9928 <pistike.kaplar@gmail.com>
2021-11-21 21:39:46 +0100

    CsvTestFragment.kt: Added File Writing

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/csv/CsvTestFragment.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/csv/CsvTestFragment.kt
@@ -1,15 +1,29 @@
 package hu.unideb.inf.mobilemeasurement.csv
 
 import android.os.Bundle
+import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import hu.unideb.inf.mobilemeasurement.R
 import hu.unideb.inf.mobilemeasurement.databinding.FragmentCsvTestBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileWriter
+import java.nio.charset.Charset
 
 class CsvTestFragment : Fragment() {
+
+    lateinit var editTextInput: EditText
+    lateinit var buttonWrite: Button
+    lateinit var buttonRead: Button
+    lateinit var textviewOutput: TextView
+    private val CSV_HEADER = "id;name"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +41,32 @@ class CsvTestFragment : Fragment() {
             container,
             false
         )
+        editTextInput = binding.editTextTextInput
+        buttonWrite = binding.buttonWrite
+        buttonRead = binding.buttonRead
+        textviewOutput = binding.textViewOutputCsv
 
+        buttonWrite.setOnClickListener{
+
+            val path = context?.getExternalFilesDir(null)
+
+            val csvsDirectory = File(path, "csvs")
+            csvsDirectory.mkdirs()
+
+            val file = File(csvsDirectory,"csv_test_output.csv")
+
+            try {
+                file.appendText(CSV_HEADER)
+                file.appendText("\n")
+                file.appendText("1;Geza\n")
+                file.appendText("2;"+editTextInput.text+"\n")
+                println("Write CSV successfully!")
+
+            } catch (e: Exception) {
+                println("Writing CSV error!")
+                e.printStackTrace()
+            }
+        }
 
         return binding.root
     }
