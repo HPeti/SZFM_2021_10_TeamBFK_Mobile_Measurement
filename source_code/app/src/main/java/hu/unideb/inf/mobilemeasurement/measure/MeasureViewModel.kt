63636477d68a517ee10280406a0d9c9981666f5f
Péter Hegedűs <hpeti0912@gmail.com>
2021-12-04 17:40:38 +0100

    Added sensorData and calculatedDistance fields to MeasureViewModel.

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureViewModel.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureViewModel.kt
@@ -2,8 +2,10 @@ package hu.unideb.inf.mobilemeasurement.measure
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MeasureStartViewModel : ViewModel(){
+class MeasureViewModel : ViewModel(){
 
     val distance: MutableLiveData<Int> by lazy{
         MutableLiveData<Int>()
@@ -21,9 +23,31 @@ class MeasureStartViewModel : ViewModel(){
         MutableLiveData<Int>()
     }
 
-    val orientation_Data: MutableLiveData<Int> by lazy{
-        MutableLiveData<Int>()
+    val orientation: MutableLiveData<String> by lazy{
+        MutableLiveData<String>()
     }
 
+    val date : MutableLiveData<Date> by lazy{
+        MutableLiveData<Date>()
+    }
+
+    val sensorData1 : MutableLiveData<ArrayList<Float>> by lazy {
+        MutableLiveData<ArrayList<Float>>()
+    }
+    val sensorData2 : MutableLiveData<ArrayList<Float>> by lazy {
+        MutableLiveData<ArrayList<Float>>()
+    }
+    val sensorData3 : MutableLiveData<ArrayList<Float>> by lazy {
+        MutableLiveData<ArrayList<Float>>()
+    }
+    val calculatedDistance1 : MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>()
+    }
+    val calculatedDistance2 : MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>()
+    }
+    val calculatedDistance3 : MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>()
+    }
 
 }
