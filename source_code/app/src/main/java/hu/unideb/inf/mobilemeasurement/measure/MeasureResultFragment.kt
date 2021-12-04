2114745a801040e115c19bde0026fca2b6c47d4e
Péter Hegedűs <hpeti0912@gmail.com>
2021-12-04 14:55:25 +0100

    Tested dateformatter and android.os.Build infos in MeasureResultFragment.kt

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureResultFragment.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureResultFragment.kt
@@ -5,12 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import hu.unideb.inf.mobilemeasurement.R
 import hu.unideb.inf.mobilemeasurement.databinding.FragmentMeasureResultBinding
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MeasureResultFragment : Fragment() {
+    private lateinit var measure1Value : TextView
+    private lateinit var measure2Value : TextView
+    private lateinit var measure3Value : TextView
+    private val phoneID : String = android.os.Build.BRAND+"_"+android.os.Build.MODEL
+    private val dateFormatter : SimpleDateFormat= SimpleDateFormat("yyyy_MM_dd_hh_mm_ss", Locale.US);
+    private val currentDate = dateFormatter.format(Calendar.getInstance().getTime())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +33,13 @@ class MeasureResultFragment : Fragment() {
             view.findNavController().navigate(MeasureResultFragmentDirections.actionMeasureResultFragmentToHomeFragment())
         }
 
+        measure1Value = binding.measure1Value
+        measure2Value = binding.measure2Value
+        measure3Value = binding.measure3Value
 
+        measure1Value.setText(phoneID)
+        //measure2Value.setText()
+        measure3Value.setText(currentDate)
         return binding.root
     }
 }
