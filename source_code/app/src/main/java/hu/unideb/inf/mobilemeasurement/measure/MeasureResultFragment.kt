7ff5141d09168552e8484c46aeb353f5780dd4de
Péter Hegedűs <hpeti0912@gmail.com>
2021-12-05 19:51:28 +0100

    Implemented viewmodel handling and viewmodelFactory in MeasureResultFragment.kt.

--- source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureResultFragment.kt
+++ source_code/app/src/main/java/hu/unideb/inf/mobilemeasurement/measure/MeasureResultFragment.kt
@@ -1,6 +1,7 @@
 package hu.unideb.inf.mobilemeasurement.measure
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,8 +11,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import hu.unideb.inf.mobilemeasurement.R
+import hu.unideb.inf.mobilemeasurement.database.SensorData
 import hu.unideb.inf.mobilemeasurement.databinding.FragmentMeasureResultBinding
-import java.text.DateFormat
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -19,7 +21,8 @@ class MeasureResultFragment : Fragment() {
     private lateinit var measure1Value : TextView
     private lateinit var measure2Value : TextView
     private lateinit var measure3Value : TextView
-    //private var viewModel: MeasureViewModel by
+    private lateinit var viewModel: MeasureViewModel
+    private lateinit var viewModelFactory: MeasureViewModelFactory
 
     private val phoneID : String = android.os.Build.BRAND+"_"+android.os.Build.MODEL
     private val dateFormatter : SimpleDateFormat= SimpleDateFormat("yyyy_MM_dd_hh_mm_ss", Locale.US);
@@ -29,8 +32,20 @@ class MeasureResultFragment : Fragment() {
         measure1Value = binding.measure1Value
         measure2Value = binding.measure2Value
         measure3Value = binding.measure3Value
-        //viewModel = ViewModelProvider(this).get(MeasureViewModel::class.java)
-        //binding.measureViewModel = viewModel
+        viewModelFactory = MeasureViewModelFactory(MeasureResultFragmentArgs.fromBundle(requireArguments()).distance,
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).measureID,
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).phoneID,
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).samplingRate,
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).orientation,
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).sensorData1!!.toCollection(ArrayList<SensorData>()),
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).sensorData2!!.toCollection(ArrayList<SensorData>()),
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).sensorData3!!.toCollection(ArrayList<SensorData>()),
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).calculatedDistance1,
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).calculatedDistance2,
+            MeasureResultFragmentArgs.fromBundle(requireArguments()).calculatedDistance3
+            )
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MeasureViewModel::class.java)
+
     }
 
     override fun onCreateView(
@@ -46,8 +61,8 @@ class MeasureResultFragment : Fragment() {
 
         setupClass(binding)
 
-        measure1Value.setText(phoneID)
-        //measure2Value.setText()
+        measure1Value.setText(viewModel.phone_ID.value.toString())
+        measure2Value.setText(viewModel.measure_ID.value.toString())
         measure3Value.setText(currentDate)
         return binding.root
     }
